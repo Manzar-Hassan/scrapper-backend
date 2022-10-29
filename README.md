1bfba78d8fd6d99bf59f4e7717ec12df49670a3c
Manzar Hassan <manzarhassan05@gmail.com>
2022-10-29 14:56:56 +0530

    arranging the output data

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NOTE
 
-please install chrome driver v-107 and update chrome to v-107 before starting the scrapper server
+please install chrome webdriver v-107 and update chrome to v-107 before starting the scrapper server
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import webdriver from "selenium-webdriver";
 import "chromedriver";
 
@@ -7,11 +8,13 @@ const url =
   "https://www.pinnacle.it/scommesse/calcio/uefa-champions-league/matchups/";
 
 //importing By for css selectors
-const { By } = webdriver;
+const { By, Builder } = webdriver;
 const PORT = process.env.PORT || 5000;
 
 //initializing the server
 const app = express();
+
+app.use(cors())
 
 //function to receive a get request from client side
 app.get("/", async (request, response) => {
@@ -28,14 +31,13 @@ app.get("/", async (request, response) => {
 //function to scrape data from url
 async function scrapeData() {
   try {
-
-    //initializing chrome webdriver 
-    var driver = new webdriver.Builder().forBrowser("chrome").build()
+    //initializing chrome webdriver
+    var driver = new Builder().forBrowser("chrome").build();
 
     //fetching url
     await driver.get(url);
 
-    //synchronizing 
+    //synchronizing
     await driver.manage().setTimeouts({ implicit: 500 });
 
     //finding the required elements by id
@@ -47,16 +49,58 @@ async function scrapeData() {
     //saving the values in an array
     const data = value.split("\n");
 
+    //initializing output array
+    let outputArrangedData = [];
+
+    //arranging output format
+    for (let i = 0; i < data.length; i++) {
+      let arrangedData = {
+        alias: 0,
+        time: "",
+        teams: "",
+        value: 0,
+        bet1: "",
+        betx: "",
+        bet2: "",
+        betx1: "",
+        bet12: "",
+        x2: "",
+        under: "",
+        over: "",
+        goal: "",
+        nogoal: "",
+      };
+
+      if (Number(data[i]) > 1000) {
+        arrangedData.alias = data[i];
+        arrangedData.time = data[i + 1];
+        arrangedData.teams = `${data[i + 2]} ${data[i + 3]} ${data[i + 4]}`;
+        arrangedData.value = data[i + 5];
+        arrangedData.bet1 = data[i + 8];
+        arrangedData.betx = data[i + 10];
+        arrangedData.bet2 = data[i + 12];
+        arrangedData.betx1 = data[i + 15];
+        arrangedData.bet12 = data[i + 17];
+        arrangedData.x2 = data[i + 19];
+        arrangedData.under = data[i + 22];
+        arrangedData.over = data[i + 24];
+        arrangedData.goal = data[i + 27];
+        arrangedData.nogoal = data[i + 29];
+
+        outputArrangedData.push(arrangedData);
+        i += 29;
+      }
+    }
+
     //quiting the browser after the task completes
     driver.quit();
 
     //returning the fetched data
-    return data;
-
+    return outputArrangedData;
   } catch (error) {
     throw new Error(error);
   }
 }
 
-//starting the server at required port
+//starting the server at required
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
